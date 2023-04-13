159aee7daf50f504721e5ac34f29401e22c58fee
Neechelle James <neechelle.james@gmail.com>
2023-04-13 10:55:08 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(x, y) {
+    return (x / y)
+}
+divide(2000, 100)
+
+const square = parameter => parameter * parameter;
+
+const add = (parameter2, parameter3) => parameter2 + parameter3;
